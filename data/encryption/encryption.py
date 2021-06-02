5ac0d3eed83865aff0f6109a1c82babfb80feeee
Pulsar <62395480+Woodnet@users.noreply.github.com>
2021-06-03 00:09:37 +0200

    Add files via upload

--- data/encryption/encryption.py
+++ data/encryption/encryption.py
@@ -7,8 +7,9 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 def create_private_key():
     nummer = random.randint(9999,999999)
     random_string = Fernet.generate_key().decode()
-    password = ("%s%s"%(random_string,nummer)).encode()
-    salt = os.urandom(16)
+    random_string_2 = Fernet.generate_key().decode()
+    password = ("%s%s%s"%(random_string,random_string_2,nummer)).encode()
+    salt = os.urandom(999999)
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
@@ -18,3 +19,7 @@ def create_private_key():
     private_key = base64.urlsafe_b64encode(kdf.derive(password))
     private_f = Fernet(private_key)
     return (private_key,private_f)
+
+(private_key,private_f) = create_private_key()
+print(len(private_key))
+print(private_key)
